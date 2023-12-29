09ccf69cf4910525a1b2ae2718de480d2bfd1328
htppsvitor <vitor.qn2004@gmail.com>
2023-12-29 01:21:58 -0300

    Style: Ajustes de responsividade

--- script.js
+++ script.js
@@ -59,3 +59,41 @@ function shuffleArray(array) {
 
     return array;
 }
+
+
+
+class Menu {
+    constructor(mobileMenu,navList,covens) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        console.log(this);
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+
+} 
+
+const menu = new Menu (
+    ".menu",
+    "header nav"
+
+)
+
+menu.init();
